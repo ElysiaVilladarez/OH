b18e1558b2a67f3cfa331753d81c46be0828041d
elysiavilladarez <elysiajelenavilladarez@yahoo.com>
2017-05-10 15:04:06 +0800

    Initial Commit

---
+++ OOHANA/app/src/main/java/com/oohana/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.oohana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.oohana.oohana.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
